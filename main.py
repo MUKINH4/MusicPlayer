27f8e0e7b8f32dbd0b6f86aa6fcacb033beff1c7
MUKINH4 <samuelmaragato3@gmail.com>
2024-03-10 16:26:05 -0300

    Some modification

--- main.py
+++ main.py
@@ -2,30 +2,26 @@ from pygame import mixer
 from tkinter import messagebox
 from tkinter import *
 from PIL import ImageTk, Image
-import json
+import os
 
-json_data = './musicas/musicas.json'
-with open(json_data, 'r') as j:
-    contents = json.loads(j.read())
+os.chdir(r'musicas')
+musicas = os.listdir()
 
-
+selected_index = 0
 
 win = Tk()
 
 win.geometry("700x500")
 
-playImage = ImageTk.PhotoImage(file='imagens/botao-play.png')
-pauseImage = ImageTk.PhotoImage(file='imagens/botao-pause.png')
-unpauseImage = ImageTk.PhotoImage(file='imagens/botao-unpause.png')
-backImage = ImageTk.PhotoImage(file='imagens/seta-para-a-esquerda.png')
-nextImage = ImageTk.PhotoImage(file='imagens/seta-para-a-direita.png')
-img = ImageTk.PhotoImage(file='imagens/YT_Music.jpg')
+playImage = ImageTk.PhotoImage(file='../imagens/botao-play.png')
+pauseImage = ImageTk.PhotoImage(file='../imagens/botao-pause.png')
+unpauseImage = ImageTk.PhotoImage(file='../imagens/botao-unpause.png')
+backImage = ImageTk.PhotoImage(file='../imagens/seta-para-a-esquerda.png')
+nextImage = ImageTk.PhotoImage(file='../imagens/seta-para-a-direita.png')
+img = ImageTk.PhotoImage(file='../imagens/YT_Music.jpg')
     
 selected_index = 0
-for index, music in enumerate(contents):
-    if music['song']:
-        selected_index = index
-        print(selected_index)                      
+                    
 
 class App(Tk):    
     def __init__(self):
@@ -33,6 +29,7 @@ class App(Tk):
         self.tela()
         self.buttons()
         self.frames_da_tela()
+        self.listas()
         win.mainloop()
     def tela(self):
         self.win.title('Music Player') 
@@ -49,26 +46,37 @@ class App(Tk):
         self.frame_2.grid(row=1, column=2, padx=10, pady=10, sticky='nsew')
         self.logo = Label(self.frame_2, image=img)
         self.logo.pack()
-        #self.frame_1.place(relx=0.02, rely=0.02, relheight=0.8, relwidth=0.96)
         
+        self.current_track = Label(self.frame_1, text='Current Track', bg='black', fg='white', width=48, height=1)
+        self.current_track.pack()
+        #self.current_track.grid(row=0, column=0, padx=10, pady=10, sticky='nsew')
+        
+    def listas(self):
+        self.listbox = Listbox(self.frame_1, bg='#31363F', fg='white')
+        self.listbox.pack()
+        self.listbox.place(relx=0.05, rely=0.05, relheight=0.9, relwidth=0.9)
+        for music in musicas:
+            self.listbox.insert(END, music)
+            self.listbox.get(ACTIVE)           
+            
     def buttons(self):
-        self.bt_play = Button(self.win, bg='#31363F', image=playImage, text='>', command=play)
+        self.bt_play = Button(self.win, bg='#31363F', image=playImage, text='>', command=play, relief='raised', overrelief='ridge')
         self.bt_play.pack()
         self.bt_play.place(relx=0.35, rely=0.88, relheight=0.08, relwidth=0.08)
         
-        self.bt_pause = Button(self.win, bg='#31363F', image=pauseImage, text='| |', command=pause)
+        self.bt_pause = Button(self.win, bg='#31363F', image=pauseImage, text='| |', command=pause, relief='raised', overrelief='ridge')
         self.bt_pause.pack()
         self.bt_pause.place(relx=0.45, rely=0.88, relheight=0.08, relwidth=0.08)
         
-        self.bt_unpause = Button(self.win, bg='#31363F',  image=unpauseImage, text='| >', command=unpause)
+        self.bt_unpause = Button(self.win, bg='#31363F',  image=unpauseImage, text='| >', command=unpause, relief='raised', overrelief='ridge')
         self.bt_unpause.pack()
         self.bt_unpause.place(relx=0.55, rely=0.88, relheight=0.08, relwidth=0.08)
         
-        self.bt_next = Button(self.win, bg='#31363F', image=nextImage, text='>>', command=next_song)
+        self.bt_next = Button(self.win, bg='#31363F', image=nextImage, text='>>', command=next_song, relief='raised', overrelief='ridge')
         self.bt_next.pack()
         self.bt_next.place(relx=0.65, rely=0.89, relheight=0.06, relwidth=0.06)
         
-        self.bt_back = Button(self.win, bg='#31363F', image=backImage, text='<<', command=back_song)
+        self.bt_back = Button(self.win, bg='#31363F', image=backImage, text='<<', command=back_song, relief='raised', overrelief='ridge')
         self.bt_back.pack()
         self.bt_back.place(relx=0.27, rely=0.89, relheight=0.06, relwidth=0.06)        
         
@@ -78,7 +86,7 @@ class App(Tk):
         
 def play():
     mixer.init()
-    song = f'musicas/{contents[selected_index]['song']}.mp3'
+    song = music
     mixer.music.load(song)
     mixer.music.play()
     if mixer.music.get_busy() == 0:
